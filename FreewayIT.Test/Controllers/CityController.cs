0e1eb4cc72d4634e901220599d3153b41b3db027
Kacper <kacper@freewayit.pl>
2025-11-04 14:09:56 +0100

    dodawanie miast

--- FreewayIT.Test/Controllers/CityController.cs
+++ FreewayIT.Test/Controllers/CityController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using FreewayIT.Test.DAL;
@@ -13,12 +14,90 @@ namespace FreewayIT.Test.Controllers
     {
         private TestFreewayITEntities db = new TestFreewayITEntities();
 
-        //
         // GET: /City/
-
         public ActionResult Index()
         {
             return View(db.Cities.ToList());
+        }
+
+        // GET: /City/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /City/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cities.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(city);
+        }
+
+        // GET: /City/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            City city = db.Cities.Find(id);
+            if (city == null)
+                return HttpNotFound();
+
+            return View(city);
+        }
+
+        // POST: /City/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(city);
+        }
+
+        // GET: /City/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            City city = db.Cities.Find(id);
+            if (city == null)
+                return HttpNotFound();
+
+            return View(city);
+        }
+
+        // POST: /City/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            City city = db.Cities.Find(id);
+            db.Cities.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
